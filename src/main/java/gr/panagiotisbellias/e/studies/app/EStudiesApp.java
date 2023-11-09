7defb1f135cacdcbb526e41de979ac4e7e352e08
Panagiotis Bellias <belliaspan@gmail.com>
2023-11-09 16:37:20 +0200

    Fix app naming

--- src/main/java/gr/panagiotisbellias/e/studies/app/EStudiesApp.java
+++ src/main/java/gr/panagiotisbellias/e/studies/app/EStudiesApp.java
@@ -1,10 +1,10 @@
-package gr.panagiotisbellias;
+package gr.panagiotisbellias.e.studies.app;
 
 /**
- * Hello world!
+ * E Studies App
  *
  */
-public class App {
+public class EStudiesApp {
 
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
