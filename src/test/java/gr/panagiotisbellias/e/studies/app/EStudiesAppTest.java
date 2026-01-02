a63ea4b63efb379df5b26b5d7ed28da372fc1eaa
Panagiotis Bellias <belliaspan@gmail.com>
2026-01-02 21:13:21 +0200

    chore: migrate from JUnit 4 to JUnit 5 (Jupiter)
    - Resolve https://app.todoist.com/app/task/upgrade-j-unit-5-6cvXx973MGwfwvqc

--- src/test/java/gr/panagiotisbellias/e/studies/app/EStudiesAppTest.java
+++ src/test/java/gr/panagiotisbellias/e/studies/app/EStudiesAppTest.java
@@ -1,8 +1,8 @@
 package gr.panagiotisbellias.e.studies.app;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for E Studies App.
